993a902d7ff1712a9a097dca9a3ad9491ed19e74
Md Rehan <rehanme125@gmail.com>
2021-06-01 11:59:50 +0530

    b1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Alert, Button } from "react-bootstrap";
 
 function App() {
   return (
     <div>
-      <h3 className="bg-primary">React</h3>
+      <h1 className="bg-primary text-light p-2">React Bootstrap</h1>
+
+      <input type="button" className="btn btn-secondary" value="Basic Button" />
+      <Button variant="secondary">React Bootstrap Button</Button>
+
+      <div className="alert alert-primary">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam,
+        officiis.
+      </div>
+
+      <Alert variant="primary">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </Alert>
     </div>
   );
 }
